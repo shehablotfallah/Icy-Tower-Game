3cc252c2e617161190d0af0f9a23ddbb882d0b57
Shehab Lotfallah <shehabw126@gmail.com>
2023-07-25 03:31:08 +0300

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float JumpSpeed = 8f;
+    private float direction = 0f;
+    private Rigidbody2D player;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
+
+    private Animator playerAnimation;
+
+    private Vector3 respawnPoint;
+    public GameObject fallDetector;
+
+    public Text scoreText;
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+        playerAnimation = GetComponent<Animator>();
+        respawnPoint = transform.position;
+        scoreText.text = "Score: " + Scoring.totalScore;
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        direction = Input.GetAxis("Horizontal");
+
+        if (direction > 0f)
+        {
+            player.velocity = new Vector2 (direction * speed, player.velocity.y);
+            transform.localScale = new Vector2(0.203043f, 0.203043f);
+
+        }
+        else if (direction < 0f)
+        {
+            player.velocity = new Vector2(direction * speed, player.velocity.y);
+            transform.localScale = new Vector2(-0.203043f, 0.203043f);
+        }
+        else
+        {
+            player.velocity = new Vector2(0, player.velocity.y);
+        }
+
+        if (Input.GetButtonDown("Jump") && isTouchingGround)
+        {
+            player.velocity = new Vector2(player.velocity.x, JumpSpeed);
+        }
+
+        playerAnimation.SetFloat("Speed", Mathf.Abs(player.velocity.x));
+        playerAnimation.SetBool("OnGround", isTouchingGround);
+
+        fallDetector.transform.position = new Vector2(transform.position.x, fallDetector.transform.position.y);
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "FallDetector")
+        {
+            transform.position = respawnPoint;
+        }
+        else if (collision.tag == "Checkpoint")
+        {
+            respawnPoint = transform.position;
+        }
+        else if (collision.tag == "NextLevel")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            respawnPoint = transform.position;
+        }
+        else if (collision.tag == "PreviousLevel")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            respawnPoint = transform.position;
+        }else if (collision.tag == "PlayAgain")
+        {
+            SceneManager.LoadScene(0);
+            respawnPoint = transform.position;
+        }
+        else if (collision.tag == "Crystal")
+        {
+            Scoring.totalScore += 1;
+            scoreText.text = "Score: " + Scoring.totalScore;
+            collision.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Spike")
+        {
+            healthBar.Damage(0.001f);
+        }
+    }
+}
+
